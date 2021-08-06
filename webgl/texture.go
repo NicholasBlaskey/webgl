e5541235220373ac5b14575c975a3083f3f679da
Nicholas Blaskey <nicholas.blaskey1@marist.edu>
2021-08-06 17:35:52 -0400

    Added texture functionaliy

---
+++ webgl/texture.go
@@ -0,0 +1,40 @@
+package webgl
+
+import (
+	"syscall/js"
+)
+
+type Texture struct {
+	JsTexture js.Value
+}
+
+func (gl *Gl) CreateTexture() *Texture {
+	t := gl.JsGl.Call("createTexture")
+	if t.IsUndefined() {
+		return nil
+	}
+	return &Texture{t}
+}
+
+func (gl *Gl) BindTexture(target int, texture *Texture) {
+	gl.JsGl.Call("bindTexture", target, texture)
+}
+
+func (gl *Gl) ActiveTexture(texUnit int) {
+	gl.JsGl.Call("activeTexture", texUnit)
+}
+
+func (gl *Gl) TexImage2DArray(target, level, internalFormat,
+	width, height, border, format, texType int, pixels interface{}) {
+
+	gl.JsGl.Call("texImage2D", target, level, internalFormat,
+		width, height, border, format, texType, toTypedArray(pixels))
+}
+
+func (gl *Gl) TexImage2D(target, level, internalFormat, format, texType int, pixels interface{}) {
+	gl.JsGl.Call("texImage2D", target, level, internalFormat, format, texType, pixels)
+}
+
+func (gl *Gl) TexParameteri(target, pname, param int) {
+	gl.JsGl.Call("texParameteri", target, pname, param)
+}
